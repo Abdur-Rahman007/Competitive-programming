321bf860d1d400347da74504ba6c6be553c843a2
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-08-30 23:26:06 +0600

    Update C_Numbers_on_Whiteboard.cpp

--- week-2/day-4/C_Numbers_on_Whiteboard.cpp
+++ week-2/day-4/C_Numbers_on_Whiteboard.cpp
@@ -11,11 +11,46 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        vector<int> v;
+        priority_queue<int> q;
+        for (int i=1; i<=n; i++)
+        {
+            q.push(i);
+        }
+        for (int i=1; i<n; i++)
+        {
+            int a = q.top();
+            q.pop();
+            int b = q.top();
+            q.pop();
+            v.push_back(a);
+            v.push_back(b);
+            q.push((a+b+1)/2);
+        }
         
+        cout << q.top() <<'\n';
+
+        int cnt = 1;
+        for (int a: v)
+        {
+            if(cnt < 3)
+            {
+                cout << a <<" ";
+                cnt++;
+            }
+            if (cnt == 3)
+            {
+                cout << '\n';
+                cnt = 1;
+            }
+        }
         
     }
     
 
-
     return 0;
+
 }
