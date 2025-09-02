cb27b161e6c676b1601a1603ba37016851d0aece
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-09-02 23:38:07 +0600

    Update D_Matryoshkas.cpp

--- week-2/day-5/D_Matryoshkas.cpp
+++ week-2/day-5/D_Matryoshkas.cpp
@@ -11,8 +11,31 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        multiset<long long int> mt;
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            mt.insert(x);
+        }
         
+        int ans = 0;
+
+        while(!mt.empty())
+        {
+            long long int x = *mt.begin();
+            while (mt.find(x) != mt.end())
+            {
+                mt.erase(mt.find(x));
+                x++;
+            }
+            ans++;
+        }
         
+        cout << ans <<'\n';
     }
     
 
