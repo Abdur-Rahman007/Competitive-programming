26df286f48983f5b5640995657953b323ce7f7ae
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-09-10 22:13:14 +0600

    Update D_Line.cpp

--- week-2/day-7/D_Line.cpp
+++ week-2/day-7/D_Line.cpp
@@ -11,8 +11,58 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        long long int cnt = 0;
+        vector<long long int>  v, ans(n+1);
+        for (int i=0; i<n; i++)
+        {
+            int L = i, R = n-i-1;
+
+            if(s[i]=='L')
+            {
+                if(R>L)
+                {
+                    cnt += R;
+                    v.push_back(R-L);
+                }
+                else
+                    cnt += L;
+            }
+            else{
+                if(L>R)
+                {
+                    cnt += L;
+                    v.push_back(L-R);
+                }
+                else
+                    cnt += R;
+            }
+        }
+
+        sort(v.begin(), v.end(), greater<long long int>());
         
         
+        for (int i=v.size(); i<=n; i++)
+        {
+            ans[i] = cnt;
+        }
+
+        for (int i=v.size()-1; i>=1; i--)
+        {
+            cnt -= v[i];
+            ans[i] = cnt;
+        }
+
+        for (int i=1; i<=n; i++)
+        {
+            cout << ans[i] <<" ";
+        }
+
+        cout <<'\n';
     }
     
 
