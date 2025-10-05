2caa06d19c79fb1b29a5b4cc5581f54e872aecd6
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-05 20:16:35 +0600

    Update C_Premutation.cpp

--- week-3/day-1/C_Premutation.cpp
+++ week-3/day-1/C_Premutation.cpp
@@ -11,8 +11,50 @@ int main()
 
     while (t--)
     {
+        int n, flag=0;
+        cin >> n;
+
+        int arr[n][n-1];
+        for(int i=0; i<n; i++)
+        {
+            for (int j=0; j<n-1; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+
+        map<int, int> mp;
+        vector<int> v;
+        for (int i=0; i<n; i++)
+        {
+            mp[arr[i][0]]++;
+            if(mp[arr[i][0]]==n-1)
+            {
+                v.push_back(arr[i][0]);
+                flag = arr[i][0];
+            }
+        }
+
+        for (int i= 0; i<n; i++)
+        {
+            if(arr[i][0]!= flag)
+            {
+                flag = i;
+                break;
+            }
+        }
         
+        for (int i=0; i<n-1; i++)
+        {
+            v.push_back(arr[flag][i]);
+        }
+
+        for (int i=0; i<n; i++)
+        {
+            cout << v[i] <<" ";
+        }
         
+        cout <<'\n';
     }
     
 
