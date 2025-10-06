dd2efb3805ed0fbe7b447e53e36d14796254713c
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-06 11:16:23 +0600

    Update D_X_Sum.cpp

--- week-3/day-2/D_X_Sum.cpp
+++ week-3/day-2/D_X_Sum.cpp
@@ -11,8 +11,66 @@ int main()
 
     while (t--)
     {
+        int n, m;
+        cin >> n >> m;
+
+        int arr[n][m];
+
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+        
+        int ans = 0;
+        
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                int sum = arr[i][j];
+                int p = i+1;
+                int q = j+1;
+                while (p<n && q<m)
+                {
+                    sum += arr[p][q];
+                    p++;
+                    q++;
+                }
+                p = i-1;
+                q = j-1;
+                while (p>=0 && q>=0)
+                {
+                    sum += arr[p][q];
+                    p--;
+                    q--;
+                }
+                p = i+1;
+                q = j-1;
+                while (p<n && q>=0)
+                {
+                    sum += arr[p][q];
+                    p++;
+                    q--;
+                }
+                p = i-1;
+                q = j+1;
+                while (p>=0 && q<m)
+                {
+                    sum += arr[p][q];
+                    p--;
+                    q++;
+                }
+                
+                ans = max(ans, sum);
+            }
+            
+        }
         
         
+        cout << ans <<'\n';
     }
 
 
