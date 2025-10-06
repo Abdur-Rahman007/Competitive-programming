decadcd039d1b8862ec555dc342a44f565f4a7fa
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-06 11:17:17 +0600

    Update EVacuate_to_Moon.cpp

--- week-3/day-2/EVacuate_to_Moon.cpp
+++ week-3/day-2/EVacuate_to_Moon.cpp
@@ -11,8 +11,29 @@ int main()
 
     while (t--)
     {
+        int n, m, h;
+        cin >> n >> m >> h;
+
+        vector<long long int> a(n), b(m);
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];   
+        }
+        for (int i=0; i<m; i++)
+        {
+            cin >> b[i];
+        }
         
+        sort(a.begin(), a.end(), greater<int>());
+        sort(b.begin(), b.end(), greater<int>());
+
+        long long int ans = 0;
+        for (int i=0; i<min(n,m); i++)
+        {
+            ans += min(a[i], b[i]*h);
+        }
         
+        cout << ans <<'\n';
     }
     
 
