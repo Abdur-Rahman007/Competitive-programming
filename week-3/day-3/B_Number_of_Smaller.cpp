9051b891c4bcbbb331f8540046c0f39b899027ef
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-07 18:38:34 +0600

    Update B_Number_of_Smaller.cpp

--- week-3/day-3/B_Number_of_Smaller.cpp
+++ week-3/day-3/B_Number_of_Smaller.cpp
@@ -6,16 +6,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    int n, m;
+    cin >> n >> m;
 
-    while (t--)
+    vector<int> a(n), b(m), ans(m);
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> a[i];
+    }
+    for (int i=0; i<m; i++)
+    {
+        cin >> b[i];
+    }
+
+    int j = 0;
+    for (int  i=0; i<m; i++)
+    {
+        while (a[j]<b[i] && (j<n))
+        {
+            j++;
+        }
+        ans[i] = j;
+    }
+
+    for (int i=0; i<m; i++)
+    {
+        cout << ans[i] << " ";
     }
     
-
-
+    
     return 0;
 }
