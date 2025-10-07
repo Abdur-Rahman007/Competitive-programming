279640749d9caa6ac3b25b5dc344dce6ef5c04e4
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-07 18:39:42 +0600

    Update C_Number_of_Equal.cpp

--- week-3/day-3/C_Number_of_Equal.cpp
+++ week-3/day-3/C_Number_of_Equal.cpp
@@ -6,16 +6,30 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    int n, m;
+    cin >> n >> m;
 
-    while (t--)
+    map<long long, long long> a, b;
+    for (int i=0; i<n; i++)
     {
-        
-        
+        long long x;
+        cin >> x;
+        a[x]++;
     }
-    
+    for (int i=0; i<m; i++)
+    {
+        long long x;
+        cin >> x;
+        b[x]++;
+    }
 
+    long long ans = 0;
+    for(auto f: a)
+    {
+        ans += (f.second * b[f.first]);
+    }
+
+    cout << ans;
 
     return 0;
 }
