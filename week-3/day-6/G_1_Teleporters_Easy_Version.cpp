4e812e2f10e45f07297e75adf632b0ef314058d6
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-10 00:47:09 +0600

    Update G_1_Teleporters_Easy_Version.cpp

--- week-3/day-6/G_1_Teleporters_Easy_Version.cpp
+++ week-3/day-6/G_1_Teleporters_Easy_Version.cpp
@@ -11,7 +11,32 @@ int main()
 
     while (t--)
     {
+        long long int n, c;
+        cin >> n >> c;
+
+        vector<long long int> v(n+1);
+        for (int i=1; i<=n; i++)
+        {
+            cin >> v[i];
+            v[i] += i; 
+        }
         
+        sort(v.begin(), v.end());
+
+        long long int ans = 0;
+
+        for (int i=1; i<=n; i++)
+        {
+            if(c>=v[i])
+            {
+                c -= v[i];
+                ans++;
+            }
+            else
+                break;
+        }
+        
+        cout << ans <<'\n';
         
     }
     
