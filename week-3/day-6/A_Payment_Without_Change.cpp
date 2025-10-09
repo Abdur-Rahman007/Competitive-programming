b88561e08ed7866903e17938a1326170eb0a09a4
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-09 17:02:52 +0600

    Update A_Payment_Without_Change.cpp

--- week-3/day-6/A_Payment_Without_Change.cpp
+++ week-3/day-6/A_Payment_Without_Change.cpp
@@ -11,8 +11,21 @@ int main()
 
     while (t--)
     {
+        long long int a, b, n, s;
+        cin >> a >> b >> n >> s;
+
+        long long int total = (a*n) + b;
+        if(total < s)
+        {
+            cout << "NO\n";
+            continue;
+        }
         
-        
+        long long int x = s%n;
+        if(x>b)
+            cout <<"NO\n";
+        else
+            cout << "YES\n";
     }
     
 
