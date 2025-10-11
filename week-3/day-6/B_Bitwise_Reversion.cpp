e288180bd7e67ebcc7d761316b40f82165aa1f8e
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-11 23:31:21 +0600

    Update B_Bitwise_Reversion.cpp

--- week-3/day-6/B_Bitwise_Reversion.cpp
+++ week-3/day-6/B_Bitwise_Reversion.cpp
@@ -11,7 +11,17 @@ int main()
 
     while (t--)
     {
+        long long int x, y, z;
+        cin >> x >> y >> z;
         
+        int a = (x&y);
+        int b = (y&z);
+        int c = (z&x);
+
+        if(a==b && b==c && c==a)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
     
 
