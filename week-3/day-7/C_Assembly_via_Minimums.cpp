12ce5453ee3145b5cc4cdf3fd6ca17291b39fd98
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-12 20:30:22 +0600

    Update C_Assembly_via_Minimums.cpp

--- week-3/day-7/C_Assembly_via_Minimums.cpp
+++ week-3/day-7/C_Assembly_via_Minimums.cpp
@@ -11,7 +11,34 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        int m = (n*(n-1))/2;
+
+        vector<int> v(m);
+        for (int i=0; i<m; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
         
+        int ind = 0, k = n-1;
+        for (int i=0; i<n-1; i++)
+        {
+            int mn = INT_MAX;
+            for (int j=0; j<k; j++)
+            {
+                mn = min(v[ind], mn);
+                ind++;
+            }
+            k--;
+            cout << mn <<" ";
+        }
+        
+        cout << v[m-1] <<" ";
+
+        cout<<'\n';
         
     }
     
