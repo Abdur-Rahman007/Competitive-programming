c4401c10cd92c427a23f4cb3e758992a30e65df1
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-15 23:54:17 +0600

    Update A_Segment_Tree_for_the_Sum.cpp

--- week-4/day-1/A_Segment_Tree_for_the_Sum.cpp
+++ week-4/day-1/A_Segment_Tree_for_the_Sum.cpp
@@ -6,12 +6,33 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    int n, t;
+    cin >> n >> t;
 
+    vector<long long int> v(n);
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
     while (t--)
     {
+        long long int a, b, c;
+        cin >> a >> b >> c;
         
+        if(a==1)
+        {
+            v[b] = c;
+        }
+        else{
+            long long int ans = 0;
+            for (int i=b; i<c; i++)
+            {
+                ans += v[i];
+            }
+            cout << ans <<'\n';
+        }
         
     }
     
