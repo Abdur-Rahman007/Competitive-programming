6f00298e2bdbcd148d392b8cf75c3b743402979b
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-16 17:24:27 +0600

    Add segment sum logic to A_Segment_with_Small_Sum.cpp
    Implement logic to find the maximum length of a segment with a sum less than or equal to k.

--- week-4/day-2/A_Segment_with_Small_Sum.cpp
+++ week-4/day-2/A_Segment_with_Small_Sum.cpp
@@ -5,17 +5,34 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int t;
-    cin >> t;
 
-    while (t--)
+    long long int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> v[i];
     }
     
+    int l=0, r=0, ans=0;
+    long long int sum=0;
 
+    while (r < n)
+    {
+        sum +=v[r];
+        if (sum<=k)
+        {
+            ans = max(ans, r-l+1);
+        }
+        else{
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+    
+    cout << ans <<'\n';
 
     return 0;
 }
