a9207873f12b90b289c59b09553c2cc09e8fb1ce
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-29 23:13:50 +0600

    Two Pointers & Sliding Window logic to count segments with small sum
    Implemented logic to count segments with a sum less than or equal to a given value.

--- week-4/day-2/C_Number_of_Segments_with_Small_Sum.cpp
+++ week-4/day-2/C_Number_of_Segments_with_Small_Sum.cpp
@@ -6,16 +6,39 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    long long int n, s;
+    cin >> n >> s;
 
-    while (t--)
+    vector<long long int> v(n);
+
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> v[i];
     }
     
+    long long int sum = 0, l=0, r=0, ans = 0;
 
+    while (r<n)
+    {
+        sum += v[r];
+        if (sum<=s)
+        {
+            ans += r-l+1;
+        }
+        else{
+            while (sum>s && l<=r)
+            {
+                sum -= v[l];
+                l++;
+            }
+            if(sum<=s)
+                ans += r-l+1;
+        }
+        
+        r++;
+    }
+    
+    cout << ans;
 
     return 0;
 }
