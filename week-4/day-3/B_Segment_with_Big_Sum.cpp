dcde8e6cb404236a7f89ebf119dfdfcf7c749882
Abdur-Rahman007 <arrahman00700@gmail.com>
2025-10-30 00:43:35 +0600

    Two Pointers & Sliding Window solution for segment with big sum problem
    Implement logic to find the minimum length of a contiguous subarray with a sum greater than or equal to a given value.

--- week-4/day-3/B_Segment_with_Big_Sum.cpp
+++ week-4/day-3/B_Segment_with_Big_Sum.cpp
@@ -6,16 +6,43 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    long long int n, s;
+    cin >> n >> s;
 
-    while (t--)
+    vector<long long int> v(n);
+
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> v[i];
     }
     
+    long long int sum = 0;
+    int l = 0, r = 0, ans = INT_MAX;
 
+    while (r<n)
+    {
+        sum += v[r];
+        if(sum>=s)
+        {
+            ans = min(ans, r-l+1);
+            sum -= v[l];
+            l++;
+            while (sum<s && r<n)
+            {
+                r++;
+                sum += v[r];
+            }
+
+            sum -= v[r];
+        }
+        else
+            r++;
+    }
+    
+    if(ans == INT_MAX)
+        cout << -1;
+    else
+        cout << ans;
 
     return 0;
 }
