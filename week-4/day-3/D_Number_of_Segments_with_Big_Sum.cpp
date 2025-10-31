972abc25d53c7ca88365a386bb01416053bcfcec
Abdur Rahman <arrahman00700@gmail.com>
2025-10-31 23:28:47 +0600

    Two Pointers & Sliding Window
    Implement logic to count segments with a sum greater than or equal to a given value.

--- week-4/day-3/D_Number_of_Segments_with_Big_Sum.cpp
+++ week-4/day-3/D_Number_of_Segments_with_Big_Sum.cpp
@@ -6,15 +6,40 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    long long int n, s;
+    cin >> n >> s;
 
-    while (t--)
+    vector<long long int> v(n);
+
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> v[i];
     }
     
+    long long int sum = 0, ans = 0;
+    int l = 0, r = 0;
+
+    while (r<n)
+    {
+        sum += v[r];
+        if(sum>=s)
+        {
+            ans += n - r; 
+            sum -= v[l];
+            l++;
+            while (sum<s && r<n)
+            {
+                r++;
+                sum += v[r];
+            }
+
+            sum -= v[r];
+        }
+        else
+            r++;
+    }
+    
+    cout << ans;
 
 
     return 0;
